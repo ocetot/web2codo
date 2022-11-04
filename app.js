671d97e26171483339411773b759eb16d9647b7c
ocetot <perisjara@gmail.com>
2022-11-03 23:54:24 -0300

    archivos con java listo

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-let precio = 600;
-document.getElementById('precio').innerHTML = precio;
+let precio = 600; // precio del producto
+document.getElementById('precio').innerHTML = precio; // total
+console.log(document.getElementById('select').value);   
+
+function btnclick(){  
+   
+    total = precio * document.getElementById('cantidad').value;
+    // descuento
+    descuento = total * document.getElementById('select').value / 100;  
+    document.getElementById('descuento').innerHTML = descuento;
+
+}
+
+function borrar(){
+    document.getElementById('cantidad').value = '';
+    document.getElementById('descuento').innerHTML = '';
+}
